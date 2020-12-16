c3ec476cf240c7bc4f7ff3bc870d31a64667a196
code-shoot <franksmithattheshoot@gmail.com>
2020-12-16 19:35:10 +0530

    adding html and css files for expense tracker project

---
+++ script.js
@@ -0,0 +1,7 @@
+let balance = document.querySelector(".balance-value")
+let income = document.querySelector(".income-value")
+let expense = document.querySelector(".expense-value")
+let input_type = document.querySelector(".input-type")
+let input_value = document.querySelector(".input-value")
+
+let add_btn = document.querySelector(".add-btn")
