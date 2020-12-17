e36592e8b7ab37ac368de4c1a3bd725fd7220ff2
code-shoot <franksmithattheshoot@gmail.com>
2020-12-17 18:44:38 +0530

    complete the button interaction

--- script.js
+++ script.js
@@ -6,15 +6,59 @@ let input_value = document.querySelector(".input-value");
 let add_btn_bx = document.querySelector(".add-btn");
 let item_container = document.querySelector(".history-container");
 
-const makeItem = () => {
+let income= 1000, expense=0, balance=0;
+
+
+let calculate = () => {
+    balance = income - expense;
+    
+    income_bx.innerHTML = `$${income}`
+    expense_bx.innerHTML = `$${expense}`
+    balance_bx.innerHTML = `$${balance}`
+}
+
+const makeItem = (type, value) => {
     const item = document.createElement('div');
     const button = document.createElement('button');
     const span1 = document.createElement('span');
     const span2 = document.createElement('span');
 
-    item.appendChild(button)
-    item.appendChild(span1)
-    item.appendChild(span2)
+    button.innerHTML = "X";
+    button.addEventListener('click', removeItem);
+    span1.innerHTML = type;
+    span2.innerHTML = value;
+
+    item.appendChild(button);
+    item.appendChild(span1);
+    item.appendChild(span2);
+    
+    item.classList.add('item');
+    (value > 0) ? item.classList.add('debit') : item.classList.add('credit');
+    button.classList.add('cross-btn');
+    span1.classList.add('expense-name');
+    span2.classList.add('expense-amount');
 
     item_container.appendChild(item)
+    return item;
 }
+
+const removeItem = (e) => {
+    console.log(e.target);
+    e.target.parentNode.remove();
+}
+
+const addItem = () => {
+    
+    if(input_type.value != "" && input_value.value != "")
+        makeItem(input_type.value, input_value.value);
+    
+    input_type.value = "";
+    input_value.value = "";
+}
+
+add_btn_bx.addEventListener('click', addItem)
+
+document.querySelectorAll('.cross-btn').forEach(btn => {
+    btn.addEventListener('click', removeItem);
+})
+
